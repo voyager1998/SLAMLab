2eb3cd8d24937310b2fb05ad08a8a4480107cf38
Kun Huang <huangkun@umich.edu>
2019-09-29 16:34:47 -0400

    some progress

--- a2_code/botlab-f19/src/slam/particle_filter.cpp
+++ a2_code/botlab-f19/src/slam/particle_filter.cpp
@@ -1,8 +1,8 @@
+#include <time.h>
 #include <cassert>
 #include <lcmtypes/pose_xyt_t.hpp>
 #include <slam/occupancy_grid.hpp>
 #include <slam/particle_filter.hpp>
-#include <time.h>
 
 using namespace std;
 
@@ -18,8 +18,8 @@ void ParticleFilter::initializeFilterAtPose(const pose_xyt_t& pose, const Occupa
     float map_height = map.heightInMeters();
     srand(time(NULL));
     random_device rd;
-    uniform_real_distribution<float> randomXY(-min(map_width, map_height) / 5.0, min(map_width, map_height) / 5.0);
-    uniform_real_distribution<float> randomTheta(-M_PI, M_PI);//???
+    normal_distribution<float> randomXY(0.0, min(map_width, map_height) / 20.0);
+    normal_distribution<float> randomTheta(0.0, M_PI / 6.0);  //???
     for (int i = 0; i < kNumParticles_; i++) {
         posterior_[i].pose.x = pose.x + randomXY(rd);
         posterior_[i].pose.y = pose.y + randomXY(rd);
@@ -58,14 +58,14 @@ particles_t ParticleFilter::particles(void) const {
     return particles;
 }
 
-std::vector<particle_t> ParticleFilter::resamplePosteriorDistribution(void) {
+std::vector<particle_t> ParticleFilter::resamplePosteriorDistribution(void) {  //action model update
     //////////// TODO: Implement your algorithm for resampling from the posterior distribution ///////////////////
 
     std::vector<particle_t> prior;
     return prior;
 }
 
-std::vector<particle_t> ParticleFilter::computeProposalDistribution(const std::vector<particle_t>& prior) {
+std::vector<particle_t> ParticleFilter::computeProposalDistribution(const std::vector<particle_t>& prior) {  //weight update based on sensormodel
     //////////// TODO: Implement your algorithm for creating the proposal distribution by sampling from the ActionModel
     std::vector<particle_t> proposal;
     return proposal;
@@ -80,8 +80,17 @@ std::vector<particle_t> ParticleFilter::computeNormalizedPosterior(const std::ve
     return posterior;
 }
 
-pose_xyt_t ParticleFilter::estimatePosteriorPose(const std::vector<particle_t>& posterior) {
+pose_xyt_t ParticleFilter::estimatePosteriorPose(const std::vector<particle_t>& posterior) {  //mean pose or maximun weight pose
     //////// TODO: Implement your method for computing the final pose estimate based on the posterior distribution
     pose_xyt_t pose;
+    float max_weight = 0.0;
+    for (auto i : posterior_) {
+        if (max_weight < i.weight) {
+            max_weight = i.weight;
+            pose.x = i.pose.x;
+            pose.y = i.pose.y;
+            pose.theta = i.pose.theta;
+        }
+    }
     return pose;
 }
