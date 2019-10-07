92e6f7d7236c5cabef991e809e825a955482fee4
Zhiqi Chen <zhiqich@umich.edu>
2019-10-06 23:27:44 -0400

    m

--- a2_code/botlab-f19/src/planning/astar.cpp
+++ a2_code/botlab-f19/src/planning/astar.cpp
@@ -10,30 +10,29 @@
 #include <stdlib.h>
 using namespace std;
 
-
 struct node
 {
     Point<int> coor;
     float fs;
-    bool operator<(const node& other) const {
+    bool operator<(const node &other) const
+    {
         return fs < other.fs;
     }
-    bool operator==(const node& other) const {
-        return coor.x == other.coor.x && coor.y == other.coor.y; 
+    bool operator==(const node &other) const
+    {
+        return coor.x == other.coor.x && coor.y == other.coor.y;
     }
 };
-
 
 struct cmp
 {
-    bool operator()(const node& a, const node& b) const
+    bool operator()(const node &a, const node &b) const
     {
-        return a.fs > b.fs ;
+        return a.fs > b.fs;
     }
 };
 
-
-robot_path_t reconstruct_path(map<Point<int>, Point<int>> cameFrom, Point<int> current, const ObstacleDistanceGrid& distances, pose_xyt_t start, pose_xyt_t goal)
+robot_path_t reconstruct_path(map<Point<int>, Point<int>> cameFrom, Point<int> current, const ObstacleDistanceGrid &distances, pose_xyt_t start, pose_xyt_t goal)
 {
     robot_path_t total_path;
     total_path.path.push_back(goal);
@@ -63,42 +62,40 @@ robot_path_t reconstruct_path(map<Point<int>, Point<int>> cameFrom, Point<int> c
     return total_path;
 }
 
-
 float heuristic(Point<int> a, Point<int> b)
 {
     srand(time(NULL));
     // float euclideanDist = sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
     // return euclideanDist;
-    float manhattanDist = abs(a.x - b.x) + abs(a.y - b.y) + ((double) rand() / (RAND_MAX));
+    float manhattanDist = abs(a.x - b.x) + abs(a.y - b.y) + ((double)rand() / (RAND_MAX));
     return manhattanDist;
 }
-
 
 bool inContainer(vector<Point<int>> ctn, Point<int> item)
 {
     bool res = false;
-    for (auto i : ctn) {
-        if (i == item) res = true;
+    for (auto i : ctn)
+    {
+        if (i == item)
+            res = true;
     }
     return res;
 }
 
-
 vector<int> nx = {1, -1, 0, 0, 1, 1, -1, -1};
 vector<int> ny = {0, 0, 1, -1, 1, -1, 1, -1};
 
-
-robot_path_t search_for_path(pose_xyt_t start, 
-                             pose_xyt_t goal, 
-                             const ObstacleDistanceGrid& distances,
-                             const SearchParams& params)
+robot_path_t search_for_path(pose_xyt_t start,
+                             pose_xyt_t goal,
+                             const ObstacleDistanceGrid &distances,
+                             const SearchParams &params)
 {
     ////////////////// TODO: Implement your A* search here //////////////////////////
-    
+
     // std::cout <<  "#################################" << std::endl;
     robot_path_t path;
     path.utime = start.utime;
-    path.path.push_back(start);    
+    path.path.push_back(start);
     path.path_length = path.path.size();
 
     Point<int> startGrid = distances.poseToCoor(start);
@@ -107,8 +104,10 @@ robot_path_t search_for_path(pose_xyt_t start,
     // cout << startGrid.x << ' ' << startGrid.y << endl;
     // cout << goalGrid.x << ' ' << goalGrid.y << endl;
     priority_queue<node, vector<node>, cmp> openSet;
-    vector<Point<int>> inOpen;
-    vector<Point<int>> closedSet;
+    // vector<Point<int>> inOpen;
+    vector<vector<int>> inOpen(distances.widthInCells, vector<int>(distances.heightInCells, 0));
+    // vector<Point<int>> closedSet;
+    vector<vector<int>> closedSet(distances.widthInCells, vector<int>(distances.heightInCells, 0));
     map<Point<int>, Point<int>> cameFrom;
     map<Point<int>, float> gScore;
     gScore[startGrid] = 0;
@@ -118,7 +117,8 @@ robot_path_t search_for_path(pose_xyt_t start,
     pos.coor = startGrid;
     pos.fs = fScore[startGrid];
     openSet.push(pos);
-    inOpen.push_back(startGrid);
+    // inOpen.push_back(startGrid);
+    inOpen[startGrid.x][startGrid.y] = 1;
     while (!openSet.empty())
     {
         Point<int> current = openSet.top().coor;
@@ -126,41 +126,47 @@ robot_path_t search_for_path(pose_xyt_t start,
         {
             return reconstruct_path(cameFrom, current, distances, start, goal);
         }
-        closedSet.push_back(current);
+        // closedSet.push_back(current);
+        closedSet[current.x][current.y] = 1;
         openSet.pop();
-        size_t idx = 0;
-        for (size_t i = 0; i < inOpen.size(); ++i) {
-            if (inOpen[i] == current) {
-                idx = i;
-                break;
-            }
-        }
-        inOpen.erase(inOpen.begin() + idx);
+        // size_t idx = 0;
+        // for (size_t i = 0; i < inOpen.size(); ++i)
+        // {
+        //     if (inOpen[i] == current)
+        //     {
+        //         idx = i;
+        //         break;
+        //     }
+        // }
+        // inOpen.erase(inOpen.begin() + idx);
+        inOpen[current.x][current.y] = 0;
         vector<Point<int>> neighbors;
         for (size_t i = 0; i < 8; i++)
         {
             Point<int> neighbor;
             neighbor.x = current.x + nx[i];
             neighbor.y = current.y + ny[i];
-            if (!distances.isCellInGrid(neighbor.x, neighbor.y)) continue;
+            if (!distances.isCellInGrid(neighbor.x, neighbor.y))
+                continue;
             if (distances(neighbor.x, neighbor.y) > params.minDistanceToObstacle + 0.2)
             {
                 neighbors.push_back(neighbor);
             }
-        }       
+        }
         for (auto neighbor : neighbors)
         {
-            if (inContainer(closedSet, neighbor))
+            // if (inContainer(closedSet, neighbor))
+            if (closedSet[neighbor.x][neighbor.y] == 1)
             {
                 continue;
             }
-            if (gScore.find(current) == gScore.end()) 
+            if (gScore.find(current) == gScore.end())
             {
                 gScore[current] = numeric_limits<float>::infinity();
                 // gScore[current] = 10000.0f;
             }
             float tentative_gScore = gScore[current] + distance_between_points(neighbor, current);
-            if (gScore.find(neighbor) == gScore.end()) 
+            if (gScore.find(neighbor) == gScore.end())
             {
                 gScore[neighbor] = numeric_limits<float>::infinity();
                 // gScore[neighbor] = 10000.0f;
@@ -170,18 +176,19 @@ robot_path_t search_for_path(pose_xyt_t start,
                 cameFrom[neighbor] = current;
                 gScore[neighbor] = tentative_gScore;
                 fScore[neighbor] = gScore[neighbor] + heuristic(goalGrid, neighbor);
-                if (!inContainer(inOpen, neighbor))
+                // if (!inContainer(inOpen, neighbor))
+                if (inOpen[neighbor.x][neighbor.y] == 0)
                 {
                     node pos;
                     pos.coor = neighbor;
                     pos.fs = fScore[neighbor];
                     openSet.push(pos);
-                    inOpen.push_back(neighbor);
+                    // inOpen.push_back(neighbor);
+                    inOpen[neighbor.x][neighbor.y] = 1;
                 }
-                
-            }   
+            }
         }
     }
-    
+
     return path;
 }
